d6cd6fcf3c2e8b2f2c296ca50c2bcae146555a3b
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-06-09 15:42:27 -0300

    flutter_gherkin finish tests

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,29 +1,77 @@
-import 'dart:async';
-import 'package:flutter_gherkin/flutter_gherkin.dart';
-import 'package:gherkin/gherkin.dart';
-import 'package:glob/glob.dart';
+import 'dart:io';
 
-import 'steps/responder_questionario.dart';
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
 
-Future<void> main() {
-  final config = FlutterTestConfiguration()
-    ..features = [Glob(r"test_driver/features/**.feature")]
-    ..reporters = [
-      ProgressReporter(),
-      TestRunSummaryReporter(),
-      // JsonReporter(path: './report.json')
-    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
-    ..stepDefinitions = [
-      InitQuiz(),
-      FillName(),
-      AnswerQuestions(),
-      AnswerLastQuestion(),
-      VerifyName(),
-      VerifyResult(),
-    ]
-    ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/app.dart"
-    // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
-    ..exitAfterTestRun = true; // set to false if debugging to exit cleanly
-  return GherkinRunner().execute(config);
+void main() {
+  FlutterDriver driver;
+  final initButton = find.byValueKey('init_button');
+  final startButton = find.byValueKey('start_button');
+  final textFieldName = find.byValueKey('text_field_name');
+  final answer1 = find.text(
+      'A) Teste baseado na análise da estrutura interna de um componente ou sistema');
+  final answer2 =
+      find.text('B) Testes de desempenho/performance/usabilidade e segurança');
+  final answer3 = find.text('B) erro');
+  final answer4 = find.text('C) 1899,1900,2004,2005');
+  final answer5 = find.text('A) Testes Regressivos');
+  final textResult = find.byValueKey('text_result');
+  final String correctAnswerText = 'Excelente!';
+
+  setUpAll(() async {
+    driver = await FlutterDriver.connect();
+  });
+
+  tearDownAll(() async {
+    if (driver != null) {
+      driver.close();
+    }
+  });
+
+  //Método para tirar screenshot
+  takeScreenshot(FlutterDriver driver, String name) async {
+    final List<int> pixels = await driver.screenshot();
+    final File file = new File("test_driver/screenshots/$name.png");
+    await file.writeAsBytes(pixels);
+  }
+
+  // test('check flutter driver health', () async {
+  //   final health = await driver.checkHealth();
+  //   expect(health.status, HealthStatus.ok);
+  // });
+
+  test('Responder as questões e visualizar o resultado Excelente', () async {
+    //Toca no botão iniciar quiz
+    await driver.tap(initButton);
+
+    //Toca no input de nome
+    await driver.tap(textFieldName);
+
+    //Preenche o nome Jhonatas no input
+    await driver.enterText('Jhonatas');
+
+    //Toca no botão começar
+    await driver.tap(startButton);
+
+    //Escolher a resposta da questão 1
+    await driver.tap(answer1);
+
+    //Escolher a resposta da questão 2
+    await driver.tap(answer2);
+
+    //Escolher a resposta da questão 3
+    await driver.tap(answer3);
+
+    //Escolher a resposta da questão 4
+    await driver.tap(answer4);
+
+    //Escolher a resposta da questão 5
+    await driver.tap(answer5);
+
+    //Tira print da tela de resultado
+    await takeScreenshot(driver, "teste_integrado");
+
+    //Verifica se apareceu o texto esperado
+    expect(await driver.getText(textResult), correctAnswerText);
+  });
 }
---
+++ test_driver/bdd_test.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+import 'package:glob/glob.dart';
+
+import 'steps/responder_questionario.dart';
+
+Future<void> main() {
+  final config = FlutterTestConfiguration()
+    ..features = [Glob(r"test_driver/features/**.feature")]
+    ..reporters = [
+      ProgressReporter(),
+      TestRunSummaryReporter(),
+      JsonReporter(path: './test_driver/report/report.json')
+    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
+    ..stepDefinitions = [
+      InitQuiz(),
+      FillName(),
+      AnswerQuestions(),
+      AnswerLastQuestion(),
+      VerifyName(),
+      VerifyResult(),
+    ]
+    ..restartAppBetweenScenarios = true
+    ..targetAppPath = "test_driver/app.dart"
+    // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+    ..exitAfterTestRun = true; // set to false if debugging to exit cleanly
+  return GherkinRunner().execute(config);
+}
